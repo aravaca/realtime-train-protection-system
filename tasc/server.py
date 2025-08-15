68ed60c4dc1628c99337f45f42970ac85d3daac2
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-08-15 23:01:14 +0900

    Update server.py

--- tasc/server.py
+++ tasc/server.py
@@ -164,7 +164,7 @@ class StoppingSim:
         # ---------- TASC ----------
         self.tasc_enabled = False
         self.manual_override = False
-        self.tasc_deadband_m = 0.25
+        self.tasc_deadband_m = 0.30
         self.tasc_hold_min_s = 0.25
         self._tasc_last_change_t = 0.0
         self._tasc_phase = "build"
