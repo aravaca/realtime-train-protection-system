371aefb8b5e4aefba376efc58c0ab1425d18a9c4
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-08-17 19:49:13 +0900

    Update server.py

--- tasc/server.py
+++ tasc/server.py
@@ -232,7 +232,7 @@ class StoppingSim:
 
         mu_corr = (mu - 1.0) * (0.03 / (0.3 - 1.0))
         
-        hist_corr = -0.1 * max(0, peak_notch - 2) - 0.05 * max(0.0, peak_dur_s)
+        hist_corr = -0.05 * max(0, peak_notch - 2) - 0.02 * max(0.0, peak_dur_s)
 
         return margin + grade_corr + mu_corr + mass_corr + hist_corr
 
