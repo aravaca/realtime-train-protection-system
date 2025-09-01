cc07a8d6e3e30882c94c1af69f9d7b45b784dc3b
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-09-01 02:18:32 +0000

    forward accel linear adj

--- tasc/static/notch.py
+++ tasc/static/notch.py
@@ -5,8 +5,8 @@ notches = np.array([0,1,2,3,4])
 # 목표 최대값
 max_accel = 0.694
 # 최소값
-min_accel = 0.15
-factor = 1.8
+min_accel = 0.10
+factor = 1.6
 
 # 정규화 0~1
 x_norm = (notches - notches[0]) / (notches[-1] - notches[0])
