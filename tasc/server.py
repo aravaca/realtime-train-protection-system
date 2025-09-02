c868017cfe02176a812f27e543db5516a451f762
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-09-02 04:45:06 +0000

    zoom block

--- tasc/server.py
+++ tasc/server.py
@@ -640,7 +640,7 @@ class StoppingSim:
 
         v_max_total = max(1e-6, self.veh.maxSpeed_kmh / 3.6)
         v_cap = v_max_total * (idx + 1) / n_notches
-        fade_start = 0.5 * v_cap
+        fade_start = 0.7 * v_cap
         #0.85 0.2
         min_factor = 0  # 캡 근처에서도 20%는 남음
 
