3499367a89b5edd1e0561f14345e3590554363c6
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-09-03 14:05:52 +0000

    brk/setup update

--- tasc/static/notch.py
+++ tasc/static/notch.py
@@ -1,25 +1,25 @@
 import numpy as np
 
 # 기존 notch 인덱스
-num_notch = 8
+num_notch = 7
 list = []
 for n in range(num_notch):
     list.append(n)
 
 notches = np.array(list)
 # 목표 최대값
-max_accel = 0.75
+max_accel = -0.3
 # max_accel *= (5/18)/
 # 최소값
-min_accel = 0
-factor = 1.2
+min_accel = -0.75
+factor = 0.8
 
 # 정규화 0~1
 x_norm = (notches - notches[0]) / (notches[-1] - notches[0])
 # 비선형 적용
 y_nonlin = min_accel + (max_accel - min_accel) * (x_norm**factor)
 
-y_nonlin = y_nonlin[::-1]
+y_nonlin = y_nonlin
 # 각 notch 간 간격
 deltas = np.diff(y_nonlin)
 
