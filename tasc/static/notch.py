38e9dd40b6322589e2f6d3ce7817fcff9a4c40bc
Hyungsuk Choi <157980478+aravaca@users.noreply.github.com>
2025-09-06 13:39:18 +0000

    tau brk update

--- tasc/static/notch.py
+++ tasc/static/notch.py
@@ -1,19 +1,18 @@
 import numpy as np
 
 # 기존 notch 인덱스
-num_notch = 7
+num_notch = 5
 list = []
 for n in range(num_notch):
     list.append(n)
 
 notches = np.array(list)
 # 목표 최대값
-max_accel = -0.3
+max_accel = -0.5
 # max_accel *= (5/18)/
 # 최소값
-min_accel = -0.75
-factor = 0.8
-
+min_accel = -1.11
+factor = 2
 # 정규화 0~1
 x_norm = (notches - notches[0]) / (notches[-1] - notches[0])
 # 비선형 적용
